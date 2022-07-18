68d59e04d1dda28f710bcb537a574d358c521a4f
Emmanueltaiwo <77551348+Emmanueltaiwo@users.noreply.github.com>
2022-07-18 13:53:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,58 @@
-
+//All EMPLOYEES VARIABLES
 let employee1El = document.getElementById("employee1-el")
 let employee2El = document.getElementById("employee2-el")
 let employee3El = document.getElementById("employee3-el")
 let employee4El = document.getElementById("employee4-el")
 
+//ALL TIME VARIABLES
 let time1El = document.getElementById("time1-el")
 let time2El = document.getElementById("time2-el")
 let time3El = document.getElementById("time3-el")
 let time4El = document.getElementById("time4-el")
 
+//ALL DATES VARIABLES
 let date1El = document.getElementById("date1-el")
 let date2El = document.getElementById("date2-el")
 let date3El = document.getElementById("date3-el")
 let date4El = document.getElementById("date4-el")
 
+//ADDING CURRENT DATE
+const date = new Date();
 
-let currentDate = "18 - 7 - 2022"
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
 
-let currentTime = 9
-let timeDetails = ":00am"
+// This arrangement can be altered based on how we want the date's format to appear.
+let currentDate = `${day}-${month}-${year}`
 
+
+//ADDING CURRENT TIME
+let time = new Date();
+let currentTime = time.getHours() + " - " + time.getMinutes() + " - " + time.getSeconds() + ("am");
+
+
+//ADDING FUNCTIONS TO EACH OF THE TIME BUTTON
 function time1() {
-    time1El.textContent = currentTime + timeDetails
+    time1El.textContent = currentTime
 }
 
 function time2() {
-    time2El.textContent = currentTime + timeDetails
+    time2El.textContent = currentTime
 }
 
 function time3() {
-    time3El.textContent = currentTime + timeDetails
+    time3El.textContent = currentTime
 }
 
 function time4() {
-    time4El.textContent = currentTime + timeDetails
+    time4El.textContent = currentTime
 }
 
+
+
+
+//ADDING FUNCTIONS TO EACH OF THE DATE BUTTON
 function date1() {
     date1El.textContent = currentDate
 }
@@ -54,7 +71,7 @@ function date4() {
 
 
 if (currentTime <= 9) {
-    alert("Wohoo! You are early for work")``
+    alert("Wohoo! You are early for work")
 }
 
 else {
